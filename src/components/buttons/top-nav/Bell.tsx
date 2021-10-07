4c6a20d21d32e5ee56d6963399477e5473c5bda8
brlaney <brlaney@outlook.com>
2021-10-06 21:20:06 -0500

    fix:bell svg path coordinates aligned

--- src/components/buttons/top-nav/Bell.tsx
+++ src/components/buttons/top-nav/Bell.tsx
@@ -1,15 +1,7 @@
 import * as React from 'react';
 import Svg, { Path } from 'react-native-svg';
 
-interface Props {
-  // Add more prop types here
-};
-
-/* 
-  stroke={props.iconState.active ? 'white' : '#C4C4C4'} 
-*/
-
-const Bell: React.FC<Props> = ({ ...props }) => {
+const Bell: React.FC<{}> = ({ ...props }) => {
   return (
     <Svg
       width='122'
@@ -24,7 +16,7 @@ const Bell: React.FC<Props> = ({ ...props }) => {
         strokeWidth='6'
       />
       <Path
-        d='M61.0405 77H33.0405C31.0405 77 32.0405 77 35.0405 75C38.0405 73 38.0405 71.5 38.0405 68.5C38.0405 66.6444 38.0405 64 38.0405 61V53C38.0405 48 38.7689 46.5638 40.5405 43.5C46.1664 33.771 56.0405 29 61.0405 29'
+        d='M61 77H33.0405C31.0405 77 32.0405 77 35.0405 75C38.0405 73 38.0405 71.5 38.0405 68.5C38.0405 66.6444 38.0405 64 38.0405 61V53C38.0405 48 38.7689 46.5638 40.5405 43.5C46.1664 33.771 56.0405 29 61.0405 29'
         stroke='white'
         strokeWidth='6'
       />
@@ -34,7 +26,7 @@ const Bell: React.FC<Props> = ({ ...props }) => {
         strokeWidth='6'
       />
       <Path
-        d='M61.0001 89C58 89 54 88 54 84C54 83 54 83 55 83H61H67C68 83.0001 68 83 68 84C68 88 64 89 61.0001 89Z'
+        d='M61 89C58 89 54 88 54 84C54 83 54 83 55 83H61H67C68 83.0001 68 83 68 84C68 88 64 89 61.0001 89Z'
         fill='white'
       />
     </Svg>
